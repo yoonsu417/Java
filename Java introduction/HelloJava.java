e855e4138053faf64071687c885a04dae70d5f8d
Kim YoonSu <121972417+yoonsu417@users.noreply.github.com>
2024-02-28 03:23:00 +0900

    HelloJava 출력
    * 자바 언어는 대소문자를 구분함
    * 파일명 = 클래스 이름
    * psvm -> public static void main(String[] args){ }
    * sout -> System.out.println();
    // 한 줄 주석
    /* */ 여러 줄 주석

---
+++ Java introduction/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava { //클래스 범위 시작
+
+    public static void main(String[] args){ //메인 메서드 범위 시작
+        System.out.println("Hello Java");
+    } //메인 메서드 범위 끝
+
+} //클래스 범위 끝
---
+++ Java introduction/HelloJava2.java
@@ -0,0 +1,13 @@
+public class HelloJava2 {
+
+    public static void main(String[] args){
+        System.out.println("Hello Java1"); //Hello Java1을 출력합니다.
+        // System.out.println("Hello Java2"); //Hello Java2을 출력합니다.
+
+        /*
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+        */
+    }
+
+}
